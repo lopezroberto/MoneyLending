73aebe9366c2ee5d4d4355ce9d30687b56145ae7
lopezrobert@outlook.com <lopezrobert@outlook.com>
2020-12-06 16:37:10 -0600

    Move the Guy class to its own file

--- MoneyLending/Program.cs
+++ MoneyLending/Program.cs
@@ -39,43 +39,4 @@ namespace MoneyLending
             }
         }
     }
-
-    class Guy
-    {
-        public string Name;
-        public int Cash;
-
-        public void WriteMyInfo()
-        {
-            Console.WriteLine(Name + " has " + Cash + " bucks.");
-        }
-
-        public int GiveCash(int amount)
-        {
-            if (amount <= 0)
-            {
-                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
-                return 0;
-            }
-            if (amount > Cash)
-            {
-                Console.WriteLine(Name + " says: " + "I don't have enough cash to give you " + amount);
-                return 0;
-            }
-            Cash -= amount;
-            return amount;
-        }
-
-        public void ReceiveCash(int amount)
-        {
-            if (amount <= 0)
-            {
-                Console.WriteLine(Name + " says: " + amount + " isn't an amount I'll take");
-            }    
-            else
-            {
-                Cash += amount;
-            }
-        }
-    }
 }
